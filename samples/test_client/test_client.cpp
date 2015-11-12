1255bd180d3247ca4b0c5c2bfec2b29072dd5350
svante karlsson <svante.karlsson@csi.se>
2015-11-12 17:33:43 +0100

    update

--- samples/test_client/test_client.cpp
+++ samples/test_client/test_client.cpp
@@ -7,15 +7,18 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/uuid/uuid_io.hpp>
 #include <phoebe/client.h>
 #include <boost/program_options.hpp>
-#include <openssl/md5.h>
 #include <avro/DataFile.hh>
 #include <avro/Generic.hh>
 #include <avro/Schema.hh>
 #include <avro/ValidSchema.hh>
 #include <csi_avro_utils/confluent_codec.h>
+#include <csi_avro_utils/utils.h>
+#include <csi_avro_utils/hive_schema.h>
 
+#include <openssl/md5.h>
 static boost::uuids::uuid get_md5(const void* data, size_t size)
 {
     MD5_CTX ctx;
@@ -24,104 +27,6 @@ static boost::uuids::uuid get_md5(const void* data, size_t size)
     boost::uuids::uuid uuid;
     MD5_Final(uuid.data, &ctx);
     return uuid;
-}
-
-boost::shared_ptr<avro::Schema> create_hive_column_schema(avro::Type t)
-{
-    //AVRO_STRING,    /*!< String */
-    //    AVRO_BYTES,     /*!< Sequence of variable length bytes data */
-    //    AVRO_INT,       /*!< 32-bit integer */
-    //    
-    //    ,     /*!< Floating point number */
-    //    AVRO_DOUBLE,    /*!< Double precision floating point number */
-    //    AVRO_BOOL,      /*!< Boolean value */
-    //    AVRO_NULL,      /*!< Null */
-
-    //    AVRO_RECORD,    /*!< Record, a sequence of fields */
-    //    AVRO_ENUM,      /*!< Enumeration */
-    //    AVRO_ARRAY,     /*!< Homogeneous array of some specific type */
-    //    AVRO_MAP,       /*!< Homogeneous map from string to some specific type */
-    //    AVRO_UNION,     /*!< Union of one or more types */
-    //    AVRO_FIXED,     /*!< Fixed number of bytes */
-
-    boost::shared_ptr<avro::Schema>  value_schema;
-    switch (t)
-    {
-    case avro::AVRO_INT:
-        value_schema = boost::make_shared<avro::IntSchema>();
-        break;
-    case avro::AVRO_LONG:
-        value_schema = boost::make_shared<avro::LongSchema>();
-        break;
-    default:
-        value_schema = boost::make_shared<avro::StringSchema>();
-        break;
-    };
-
-    /* Make a union of value_schema with null. Some types are already a union,
-    * in which case they must include null as the first branch of the union,
-    * and return directly from the function without getting here (otherwise
-    * we'd get a union inside a union, which is not valid Avro). */
-    boost::shared_ptr<avro::Schema> null_schema = boost::make_shared<avro::NullSchema>();
-    boost::shared_ptr<avro::UnionSchema> union_schema = boost::make_shared<avro::UnionSchema>();
-    union_schema->addType(*null_schema);
-    union_schema->addType(*value_schema);
-    return union_schema;
-}
-
-boost::shared_ptr<avro::ValidSchema>  get_key_schema(const avro::Name base_name, const std::vector<std::string>& keys, bool allow_null, const avro::ValidSchema& src_schema)
-{
-    auto r = src_schema.root();
-    assert(r->type() == avro::AVRO_RECORD);
-    size_t nr_of_leaves = r->leaves();
-
-    boost::shared_ptr<avro::RecordSchema> key_schema = boost::make_shared<avro::RecordSchema>(base_name.fullname() + "_key");
-    for (std::vector<std::string>::const_iterator i = keys.begin(); i != keys.end(); ++i)
-    {
-        for (size_t j = 0; j != nr_of_leaves; ++j)
-        {
-            auto name = r->nameAt(j);
-            if (name == *i)
-            {
-                auto l = r->leafAt(j);
-                key_schema->addField(*i, *create_hive_column_schema(l->type()));
-                break;
-            }
-        }
-    }
-    return boost::make_shared<avro::ValidSchema>(*key_schema);
-}
-
-boost::shared_ptr<avro::GenericDatum> get_key(avro::GenericDatum& value_datum, const avro::ValidSchema& key_schema)
-{
-    boost::shared_ptr<avro::GenericDatum> key = boost::make_shared<avro::GenericDatum>(key_schema);
-    assert(key_schema.root()->type() == avro::AVRO_RECORD);
-    assert(value_datum.type() == avro::AVRO_RECORD);
-    size_t nKeyFields = key_schema.root()->leaves();
-
-    avro::GenericRecord& key_record(key->value<avro::GenericRecord>());
-    avro::GenericRecord& value_record(value_datum.value<avro::GenericRecord>());
-
-    //size_t nValueFields = value_record.fieldCount();
-
-    for (int i = 0; i < nKeyFields; i++)
-    {
-        std::string column_name = key_schema.root()->nameAt(i);
-        assert(value_record.hasField(column_name));
-        assert(key_record.hasField(column_name));
-        // we need to handle keys with or without nulls ie union or value
-        if (key_record.field(column_name).type() == avro::AVRO_UNION)
-        {
-            key_record.field(column_name) = value_record.field(column_name);
-        }
-        else
-        {
-            avro::GenericUnion& au(value_record.field(column_name).value<avro::GenericUnion>());
-            avro::GenericDatum& actual_column_value = au.datum();
-            key_record.field(column_name) = actual_column_value;
-        }
-    }
-    return key;
 }
 
 struct sort_functor
@@ -284,7 +189,10 @@ int main(int argc, char** argv)
         avro::DataFileReader<avro::GenericDatum> dfr(i->generic_string().c_str());
         const avro::ValidSchema& schema = dfr.dataSchema();
 
-        auto keyschema = get_key_schema(key_schema_name, keys, false, dfr.dataSchema());
+        auto keyschema = csi::avro_hive::get_key_schema(key_schema_name, keys, false, dfr.dataSchema());
+
+        std::cerr << to_string(*keyschema) << std::endl;
+
         std::string val_schema_name = schema.root()->name().fullname();
 
         //std::string val_schema_name = schema.root()->name().fullname();
@@ -292,6 +200,8 @@ int main(int argc, char** argv)
         //std::cout << "val schema name : " << val_schema_name << std::endl;
 
         BOOST_LOG_TRIVIAL(trace) << "registering schemas";
+
+
         auto key_res = avro_codec.put_schema(key_schema_name, keyschema);
         if (key_res.first != 0)
         {
@@ -307,7 +217,7 @@ int main(int argc, char** argv)
         }
         int32_t key_id = key_res.second;
         int32_t val_id = val_res.second;
-        BOOST_LOG_TRIVIAL(info) << "registering schemas done";
+        BOOST_LOG_TRIVIAL(info) << "registering schemas done keyid:" << key_id << " val id: " << val_id;
 
         try
         {
@@ -315,7 +225,7 @@ int main(int argc, char** argv)
             std::vector<boost::uuids::uuid> messages;
             while (dfr.read(datum))
             {
-                auto key = get_key(datum, *keyschema);
+                auto key = csi::avro_hive::get_key(datum, *keyschema);
                 //encode key
                 {
                     auto os = avro_codec.encode_nonblock(key_id, key);
@@ -326,6 +236,8 @@ int main(int argc, char** argv)
                     bytes.resize(sz);
                     stream_reader.readBytes(bytes.data(), sz);
                     messages.push_back(get_md5(bytes.data(), bytes.size()));
+
+                    BOOST_LOG_TRIVIAL(info) << "requesting " << to_string(get_md5(bytes.data(), bytes.size()));
                 }
 
                 /*
@@ -341,7 +253,7 @@ int main(int argc, char** argv)
                 }
                 */
 
-                if (messages.size() > 100)
+                if (messages.size() > 1)
                 {
                     auto result = kv_store.get(messages);
                     if (result.first)
@@ -355,6 +267,9 @@ int main(int argc, char** argv)
 
                     BOOST_LOG_TRIVIAL(info) << "got " << result.second->records[0].value.size() << "bytes ";
                     messages.clear();
+
+                    //for debugging
+                    break;
                 }
 
 
